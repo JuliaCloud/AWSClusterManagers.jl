59ef53fd4e7bbab8d86572ebf4cdcaa9a7086e04
Curtis Vogt <curtis.vogt@invenia.ca>
2016-12-06 08:11:25 -0600

    Improvements to spawn_broker in tests
    - Ensures that the port is free
    - Avoid waiting forever for the broker port to listen if the process
      fails

--- test/overlay.jl
+++ test/overlay.jl
@@ -26,13 +26,21 @@ end
 Base.get_next_pid() = get_next_pid()
 
 function spawn_broker(; self_terminate=true)
+    # Ensure that broker port is free. Faster than waiting for the spawned process to fail.
+    try
+        server = listen(DEFAULT_PORT)
+        close(server)
+    catch
+        error("Unable to spawn broker: port $DEFAULT_PORT already in use")
+    end
+
     broker = Base.spawn(`$(Base.julia_cmd()) -e "using AWSClusterManagers.OverlayManagers; start_broker(self_terminate=$self_terminate)"`)
 
     # Wait until the broker is ready
     # TODO: Find better way of waiting for broker to be connectable
-    while true
+    while process_running(broker)
         try
-            sock = connect(2000)
+            sock = connect(DEFAULT_PORT)
             close(sock)
             break
         catch
