02467090649b498ab29cfe628dcc957f81db84c9
Curtis Vogt <curtis.vogt@invenia.ca>
2017-04-20 17:45:29 -0500

    Add batch tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ import Lumberjack: remove_truck
 remove_truck("console")  # Disable logging
 
 # include("ecs.jl")
+include("batch.jl")
 
 # include("overlay_managers/util.jl")
 # include("overlay_managers/transport.jl")
