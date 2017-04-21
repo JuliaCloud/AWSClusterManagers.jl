93bf553358bc1eddffc4f21d8b5e073ee37f70b5
Curtis Vogt <curtis.vogt@invenia.ca>
2017-04-21 12:07:04 -0500

    Rename batch-job to job

--- src/AWSClusterManagers.jl
+++ src/AWSClusterManagers.jl
@@ -5,7 +5,7 @@ import Base: launch, manage, cluster_cookie
 export ECSManager, AWSBatchManager
 export AWSBatchOverlayManager, LocalOverlayManager
 
-include("batch-job.jl")
+include("job.jl")
 include("container.jl")
 include("ecs.jl")
 include("batch.jl")
