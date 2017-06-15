ee56de193eed05cf4b0cf21f95ad6e29d18def22
rofinn <rory.finnegan@gmail.com>
2017-06-15 17:04:01 -0500

    Cleaned up online tests.
    - Merged batch-online into batch tests.
    - Merged mock.jl and TestHelpers.jl into testutils.jl
    - Added requiring a AWS_ONLINE variable for performing online tests.

--- test/batch.jl
+++ test/batch.jl
@@ -79,7 +79,7 @@ const BATCH_ENVS = (
 
             # Mock being run on an AWS batch job
             withenv(BATCH_ENVS...) do
-                patch = @patch readstring(cmd::AbstractCmd) = Mock.readstring(cmd)
+                patch = @patch readstring(cmd::AbstractCmd) = TestUtils.readstring(cmd)
 
                 apply(patch) do
                     job = AWSBatchJob()
@@ -101,7 +101,7 @@ const BATCH_ENVS = (
     @testset "Adding procs" begin
         @testset "Worker Succeeds" begin
             withenv(BATCH_ENVS...) do
-                patch = @patch readstring(cmd::AbstractCmd) = Mock.readstring(cmd)
+                patch = @patch readstring(cmd::AbstractCmd) = TestUtils.readstring(cmd)
 
                 apply(patch) do
                     # Bring up a single "worker"
@@ -113,12 +113,89 @@ const BATCH_ENVS = (
         end
         @testset "Worker Timeout" begin
             withenv(BATCH_ENVS...) do
-                patch = @patch readstring(cmd::AbstractCmd) = Mock.readstring(cmd, false)
+                patch = @patch readstring(cmd::AbstractCmd) = TestUtils.readstring(cmd, false)
 
                 apply(patch) do
                     @test_throws ErrorException addprocs(AWSBatchManager(1; timeout=1.0))
                 end
             end
+        end
+    end
+    @testset "Online" begin
+        online() do
+            info("Registering AWS batch job definition: $(JOB_DEFINITION.name)")
+            num_workers = 3
+
+            # Will be running the HEAD revision of the code remotely
+            # Note: Pkg.checkout doesn't work on untracked branches / SHAs with Julia 0.5.1
+            code = """
+            Pkg.update()
+            Pkg.clone("git@gitlab.invenia.ca:invenia/AWSClusterManagers.jl")
+            cd(Pkg.dir("AWSClusterManagers"))
+            run(`git checkout --detach $REV`)
+            Pkg.resolve()
+            Pkg.build("AWSClusterManagers")
+
+            using Memento
+            Memento.config("debug"; fmt="{msg}")
+            import AWSClusterManagers: AWSBatchManager
+            addprocs(AWSBatchManager($num_workers, queue="$WORKER_JOB_QUEUE"))
+            println("NumProcs: ", nprocs())
+            for i in workers()
+                println("Worker \$i: ", remotecall_fetch(() -> ENV["AWS_BATCH_JOB_ID"], i))
+            end
+            """
+
+            json = Dict(
+                "image" => "292522074875.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_DEFINITION:latest",
+                "jobRoleArn" => "arn:aws:iam::292522074875:role/AWSBatchClusterManagerJobRole",
+                "vcpus" => 1,
+                "memory" => 1024,
+                "command" => [
+                    "julia", "-e", replace(code, r"\n+", "; ")
+                ]
+            )
+
+            job_def = register(JOB_DEFINITION, json)
+
+            info("Submitting AWS Batch job")
+            job = submit(job_def, JOB_NAME, MANAGER_JOB_QUEUE)
+
+            # If no resources are available it could take around 5 minutes before the job is running
+            info("Waiting for AWS Batch job $(job.id) to complete (~5 minutes)")
+            while status(job) <= Running
+                sleep(30)
+            end
+
+            # Remove the job definition as it is specific to a revision
+            deregister(job_def)
+
+            @test status(job) == Succeeded
+
+            output = log(job)
+
+            m = match(r"(?<=NumProcs: )\d+", output)
+            num_procs = m !== nothing ? parse(Int, m.match) : -1
+            spawned_jobs = Set(matchall(r"(?<=Spawning job: )[0-9a-f\-]+", output))
+            reported_jobs = Set(matchall(r"(?<=Worker \d: )[0-9a-f\-]+", output))
+
+            @test num_procs == num_workers + 1
+            @test length(reported_jobs) == num_workers
+            @test spawned_jobs == reported_jobs
+
+            # Report some details about the job
+            d = details(job)
+            created_at = Dates.unix2datetime(d["createdAt"] / 1000)
+            started_at = Dates.unix2datetime(d["startedAt"] / 1000)
+            stopped_at = Dates.unix2datetime(d["stoppedAt"] / 1000)
+
+            # TODO: Unless I'm forgetting something just extrating the seconds from the milliseconds
+            # is awkward
+            launch_duration = div(Dates.value(started_at - created_at), 1000)
+            run_duration = div(Dates.value(stopped_at - started_at), 1000)
+
+            info("Job launch duration: $(time_str(launch_duration))")
+            info("Job run duration:    $(time_str(run_duration))")
         end
     end
 end
