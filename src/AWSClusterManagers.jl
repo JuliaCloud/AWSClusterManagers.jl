ccf59fbaff735d8866482938ee893c0e1a26eeaf
Curtis Vogt <curtis.vogt@invenia.ca>
2018-02-14 13:36:12 -0600

    Use __MODULE__
    Added in Compat 0.26

--- src/AWSClusterManagers.jl
+++ src/AWSClusterManagers.jl
@@ -3,10 +3,11 @@ module AWSClusterManagers
 import Base: launch, manage, cluster_cookie
 using Memento
 using Mocking
+using Compat: @__MODULE__
 
 export ECSManager, AWSBatchManager, DockerManager
 
-logger = get_logger(current_module())
+logger = get_logger(@__MODULE__)
 
 include("job.jl")
 include("container.jl")
