df20c1279395a817dc69b713799ed9bd342275bd
Curtis Vogt <curtis.vogt@invenia.ca>
2019-09-18 13:35:50 -0500

    Add debug messages for multi-node worker launch

--- src/batch_node.jl
+++ src/batch_node.jl
@@ -27,14 +27,19 @@ function Distributed.launch(manager::AWSBatchNodeManager, params::Dict, launched
     num_workers = manager.num_workers
     connected_workers = 0
 
+    debug(LOGGER, "Awaiting connections")
+
     # We cannot listen only to the `ENV["AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS"]` as
     # this variable is not available on the main node.
     server = listen(ip"::", AWS_BATCH_JOB_NODE_PORT)
+    debug(LOGGER, "Manager accepting worker connections on port $AWS_BATCH_JOB_NODE_PORT")
 
     while isopen(server) && connected_workers < num_workers
         # TODO: Potential issue with a random connection consuming a worker slot?
         sock = accept(server)
         connected_workers += 1
+
+        debug(LOGGER, "Worker connected")
 
         # Send the cluster cookie to the worker
         println(sock, cluster_cookie())
